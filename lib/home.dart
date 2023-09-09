47cb6a656bfd699badddfc88b00b22b06b3e14d0
AddNet <91474646+AddNet22@users.noreply.github.com>
2023-09-09 14:27:26 +0200

    BaseApp

---
+++ lib/home.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Bar du dessus
+        title: const Text('Application test'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+          child: Column(
+        children: [
+          Container(
+            color: Colors.red,
+            height: 500,
+          ),
+          Container(
+            color: Colors.blue,
+            height: 10,
+          ),
+        ],
+      )),
+      bottomNavigationBar: NavigationBar(),
+    );
+  }
+
+  Widget NavigationBar() {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      items: const [
+        BottomNavigationBarItem(
+          label: 'Home',
+          icon: Icon(Icons.access_alarm),
+        ),
+        BottomNavigationBarItem(
+          label: 'Home',
+          icon: Icon(Icons.access_alarm),
+        ),
+      ],
+    );
+  }
+}
